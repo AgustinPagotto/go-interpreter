a8adb1e9d5a0ed0a7f18f103dbc6de7d9c6290df
Agustin Pagotto <agustinpagotto@gmail.com>
2025-10-10 22:07:26 -0300

    add the e in REPL,  evaluator now evaluates literal integers and booleans

--- repl/repl.go
+++ repl/repl.go
@@ -5,11 +5,25 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/AgustinPagotto/go-interpreter/evaluator"
 	"github.com/AgustinPagotto/go-interpreter/lexer"
 	"github.com/AgustinPagotto/go-interpreter/parser"
 )
 
 const PROMPT = ">> "
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -27,11 +41,18 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
+
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
